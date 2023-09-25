7e1a4fb483f1b6f61157df6225a5ab6f45b95e28
FN LN <lucas87t@gmail.com>
2023-09-25 16:09:52 -0500

    Moved Strings

---
+++ ResistanceHR/Strings/CExperienceType.cs
@@ -0,0 +1,37 @@
+﻿using ResistanceHR.Conduct;
+using ResistanceHR.Interaction;
+using System.Collections.Generic;
+
+namespace ResistanceHR
+{
+	public static class CExperienceType
+	{
+		public const string
+			// Level Ends
+			AngeredMany = "AngeredMany",
+			FailedBigQuestDistrict = "BQMalusDowntown",
+			FailedBigQuestFloor = "BQMalusFloor",
+			FailedBigQuestGame = "BQMalusGame",
+			ElectabilityPenalty = "ElectabilityPenalty",
+			FreePrisonerFailure = "FreePrisonerFailure",
+			SetOffAlarms = "SetOffAlarms",
+			StoleNone = "StoleNone",
+			TookLotsOfDamage = "TookLotsOfDamage";
+
+		public static Dictionary<string, int> CustomXPValues = new Dictionary<string, int>()
+		{
+			{ Boozer_Schmoozer.DrankBooze, 50 },	//	√
+			{ Pyrokinetic_Learning_Style.WatchBurn,          1 },		//	√
+			{ Smoker_Schmoozer.Smoked,   100 },		//	√
+			{ AngeredMany,              -100 },     //	T
+			{ ElectabilityPenalty,      -100 },		//	C?
+			{ FailedBigQuestDistrict,   -500 },		//	C
+			{ FailedBigQuestFloor,      -300 },		//	√
+			{ FailedBigQuestGame,       -1000 },    //	C
+			{ FreePrisonerFailure,      -100 },     //	T
+			{ SetOffAlarms,             -100 },		//	C
+			{ StoleNone,                 300 },		//	C?
+			{ TookLotsOfDamage,         -100 }		//	T
+		};
+	}
+}
---
+++ ResistanceHR/Strings/CNameInterface.cs
@@ -0,0 +1,24 @@
+﻿using RogueLibsCore;
+
+namespace ResistanceHR
+{
+	public static class CNameInterface
+	{
+		[RLSetup]
+		public static void Setup()
+		{
+			// TODO: Just distribute these to their systems
+			string t = VNameType.Interface;
+			RogueLibs.CreateCustomName(CExperienceType.AngeredMany, t, new CustomNameInfo("Angered Many"));
+			RogueLibs.CreateCustomName(CExperienceType.FailedBigQuestDistrict, t, new CustomNameInfo("Failed Big Quest (District)"));
+			RogueLibs.CreateCustomName(CExperienceType.FailedBigQuestFloor, t, new CustomNameInfo("Failed Big Quest (Floor)"));
+			RogueLibs.CreateCustomName(CExperienceType.FailedBigQuestGame, t, new CustomNameInfo("Failed Big Quest (Final)"));
+			RogueLibs.CreateCustomName(CExperienceType.StoleNone, t, new CustomNameInfo("Stole Nothing"));
+			RogueLibs.CreateCustomName(CExperienceType.TookLotsOfDamage, t, new CustomNameInfo("Took Lots of Damage"));
+		}
+
+		public const string
+
+			NoMoreSemicolon = "";
+	}
+}
